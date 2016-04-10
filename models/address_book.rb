c0f1712f2104c8cd5cdaf1672f56cbb05e5344cc
Geoff Miles <miles.geoff@gmail.com>
2016-04-10 15:13:44 -0400

    Checkpoint-22: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -22,13 +22,39 @@ class AddressBook
     entries.insert(index, Entry.new(name, phone_number, email))
   end
 
-    def import_from_csv(file_name)
-      csv_text = File.read(file_name)
-      csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
 
-      csv.each do |row|
+    csv.each do |row|
       row_hash = row.to_hash
-    add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
+  end
+
+  # Search AddressBook for a specific entry by name
+  def binary_search(name)
+    # #1
+  lower = 0
+  upper = entries.length - 1
+
+  # #2
+  while lower <= upper
+    # #3
+    mid = (lower + upper) / 2
+    mid_name = entries[mid].name
+
+    # #4
+    if name == mid_name
+      return entries[mid]
+    elsif name < mid_name
+      upper = mid - 1
+    elsif name > mid_name
+      lower = mid + 1
+    end
+  end
+
+  # #5
+    return nil
   end
 end
