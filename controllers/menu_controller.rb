856d54093638a491d238c38273db939661544775
Geoff Miles <miles.geoff@gmail.com>
2016-04-10 15:58:51 -0400

    Checkpoint 23 Complete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -101,9 +101,40 @@ def create_entry
 end
 
 def search_entries
+ print "Search by name: "
+ name = gets.chomp
+ # #10
+ match = address_book.binary_search(name)
+ system "clear"
+ # #11
+ if match
+   puts match.to_s
+   search_submenu(match)
+ else
+   puts "No match found for #{name}"
+ end
 end
 
 def read_csv
+  print "Enter CSV file to import: "
+ file_name = gets.chomp
+
+ # #2
+ if file_name.empty?
+   system "clear"
+   puts "No CSV file read"
+   main_menu
+ end
+
+ # #3
+ begin
+   entry_count = address_book.import_from_csv(file_name).count
+   system "clear"
+   puts "#{entry_count} new entries added from #{file_name}"
+ rescue
+   puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+   read_csv
+ end
 end
 
 def entry_submenu(entry)
@@ -121,7 +152,10 @@ def entry_submenu(entry)
       when "n"
     # #19
       when "d"
+        delete_entry(entry)
       when "e"
+        edit_entry(entry)
+        entry_submenu(entry)
     # #20
       when "m"
         system "clear"
@@ -132,4 +166,25 @@ def entry_submenu(entry)
         entry_submenu(entry)
     end
   end
-end
+   def delete_entry(entry)
+     address_book.entries.delete(entry)
+     puts "#{entry.name} has been deleted"
+   end
+   def edit_entry(entry)
+      # #4
+      print "Updated name: "
+      name = gets.chomp
+      print "Updated phone number: "
+      phone_number = gets.chomp
+      print "Updated email: "
+      email = gets.chomp
+      # #5
+      entry.name = name if !name.empty?
+      entry.phone_number = phone_number if !phone_number.empty?
+      entry.email = email if !email.empty?
+      system "clear"
+      # #6
+      puts "Updated entry:"
+      puts entry
+    end
+ end
